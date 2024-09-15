5ca264a8f00753b0ef60d6496611bad6bb6016ce
thanhdatvo <vo9312@gmail.com>
2024-09-15 02:49:27 -0700

    rm obsession

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Enable true color support
+if vim.fn.has("termguicolors") == 1 then
+  vim.opt.termguicolors = true
+end
+
+-- Tmux clipboard integration
+vim.opt.clipboard = "unnamedplus"
