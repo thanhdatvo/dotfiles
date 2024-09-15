7686dc58e04c6931673c862559664a354898266a
thanhdatvo <vo9312@gmail.com>
2024-09-15 02:08:30 -0700

    lazy vim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfly",
+    },
+  },
+}
---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "xiyaowong/nvim-transparent",
+    opts = {
+      extra_groups = {
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "NormalFloat",
+      },
+    },
+  },
+}
