a1f921041ef63bb988a25d07b4a0adaeb92d3e98
thanh-vo-opun-ai <thanh.vo@opun.ai>
2024-10-01 18:17:11 -0700

    add replace function

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,5 +4,13 @@ return {
     opts.window.mappings = {
       ["<Tab>"] = "toggle_node", -- Use Tab to expand/collapse folder
     }
+    opts.event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function(arg)
+          vim.opt.relativenumber = true
+        end,
+      },
+    }
   end,
 }
