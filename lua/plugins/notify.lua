394a04c1e4be07272e392fb42253446bf1a58a3d
thanh-vo-opun-ai <thanh.vo@opun.ai>
2024-11-08 20:33:59 -0800

    add

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,9 +3,9 @@ return {
   "nvim-notify",
   opts = {
     stages = "fade_in_slide_out",
-    timeout = false,
-    -- max_height = 10,
-    -- max_width = 50,
+    timeout = 3000,
+    max_height = 20,
+    max_width = 50,
     top_down = false,
     background_colour = "#000000",
   },
