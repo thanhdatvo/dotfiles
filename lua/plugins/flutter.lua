67b15a1f88dfc110b56986f669708f0ba724c01c
thanh-vo-opun-ai <thanh.vo@opun.ai>
2024-11-09 14:12:34 -0800

    add

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -5,5 +5,19 @@ return {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim", -- optional for vim.ui.select
   },
-  config = true,
+  config = function()
+    require("flutter-tools").setup({
+      debugger = {
+        enabled = true,
+        register_configurations = function(_)
+          require("dap").configurations.dart = {}
+          require("dap.ext.vscode").load_launchjs()
+        end,
+      },
+      dev_log = {
+        enabled = true,
+        open_cmd = "tabedit", -- command to use to open the log buffer
+      },
+    })
+  end,
 }
