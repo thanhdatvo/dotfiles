53cd49b70052e332f2546b7afbb5733920fe2b4d
thanh-vo-opun-ai <thanh.vo@opun.ai>
2025-03-31 08:57:30 -0700

    from macos

--- lua/plugins/dadbob-ui.lua
+++ lua/plugins/dadbob-ui.lua
@@ -1,17 +1,49 @@
 return {
-  "kristijanhusak/vim-dadbod-ui",
-  dependencies = {
-    { "tpope/vim-dadbod", lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+
+      -- Add Tree-sitter support for DBUI
+      -- vim.api.nvim_create_autocmd("FileType", {
+      --   pattern = "dbui",
+      --   callback = function()
+      --     vim.treesitter.start(0, "sql")
+      --   end,
+      -- })
+      -- No need treesitter for result table
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dbout",
+        callback = function()
+          vim.treesitter.stop(0)
+        end,
+      })
+    end,
   },
-  cmd = {
-    "DBUI",
-    "DBUIToggle",
-    "DBUIAddConnection",
-    "DBUIFindBuffer",
+  { -- optional saghen/blink.cmp completion source
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+          sql = { "snippets", "dadbod", "buffer" },
+        },
+        -- add vim-dadbod-completion to your completion providers
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
   },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
 }
