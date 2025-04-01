8e5cde29ad2e285a7005374d6462ab35c0d13bc7
thanh-vo-opun-ai <thanh.vo@opun.ai>
2025-03-31 18:18:46 -0700

    from macos

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,31 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_augroup("ModeBackground", { clear = true })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = "ModeBackground",
+  pattern = "*:n",
+  command = "highlight Normal guibg=NONE",
+})
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = "ModeBackground",
+  pattern = "*:i",
+  command = "highlight Normal guibg=#005f5f",
+})
+
+-- Set Visual mode background
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = "ModeBackground",
+  pattern = "*:v",
+  command = "highlight Normal guibg=#5f0000",
+})
+
+-- cannot change Command mode background
+-- because of noice
+-- vim.api.nvim_create_autocmd("ModeChanged", {
+--   group = "ModeBackground",
+--   pattern = "*:c",
+--   command = "highlight Normal guibg=#5f5f00",
+-- })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,33 +11,3 @@ end
 vim.opt.clipboard = "unnamedplus"
 
 vim.g.nightflyWinSeparator = 2
-
-vim.api.nvim_create_augroup("ModeBackground", { clear = true })
-
--- Set Normal mode background
-vim.api.nvim_create_autocmd("ModeChanged", {
-  group = "ModeBackground",
-  pattern = "*:n",
-  command = "highlight Normal guibg=#1e1e1e",
-})
-
--- Set Insert mode background
-vim.api.nvim_create_autocmd("ModeChanged", {
-  group = "ModeBackground",
-  pattern = "*:i",
-  command = "highlight Normal guibg=#005f5f",
-})
-
--- Set Visual mode background
-vim.api.nvim_create_autocmd("ModeChanged", {
-  group = "ModeBackground",
-  pattern = "*:v",
-  command = "highlight Normal guibg=#5f0000",
-})
-
--- Set Command mode background
-vim.api.nvim_create_autocmd("ModeChanged", {
-  group = "ModeBackground",
-  pattern = "*:c",
-  command = "highlight Normal guibg=#5f5f00",
-})
