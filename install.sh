f1fce2c4cf6f1f3703bce9318fac10bc6b3a9591
Thanh Dat Vo <vo9312@gmail.com>
2026-04-16 22:43:43 -0700

    update status bar background transparent

--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
-# #!/bin/bash
-#
-# ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-# ln -sf ~/dotfiles/nvim ~/.config/nvim
-# ln -sf ~/dotfiles/tmux ~/.config/tmux
+#!/bin/bash
+
+# install node
+brew install nvm
+source ~/.zshrc
+nvm install --lts # Install latest stable Node
+nvm use --lts     # Switch to it
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install python
+brew install uv
+uv python install 3.12
+uv python pin 3.12
+
+#install go
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+gvm install go1.26.2 -B
+gvm use go1.26.2 --default
+
+#install nix??
+# sh <(curl -L https://nixos.org/nix/install) --daemon
+
+# other utility tool
+brew install zoxide
+brew install pyenv
+brew install lazygit
+brew install fzf
+brew install gitmux
