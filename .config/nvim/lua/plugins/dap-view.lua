6f7e59015af44807da3233df904fbf68c66f6060
Thanh Dat Vo <vo9312@gmail.com>
2026-07-30 20:58:31 -0700

    merge

--- .config/nvim/lua/plugins/dap-view.lua
+++ .config/nvim/lua/plugins/dap-view.lua
@@ -4,7 +4,6 @@ return {
   branch = "feature/preserve-cursor-on-tab-switch",
   -- "igorlfs/nvim-dap-view",
 
-  enabled = false,
   -- name = "nvim-dap-view",
   -- dir = "~/Work/lua-projects/nvim-dap-view",
   dependencies = {
